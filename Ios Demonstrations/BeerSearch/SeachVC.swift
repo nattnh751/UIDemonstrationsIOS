0febffc7eac61e7624b5162440a4e10bf5c3f671
Nathan Walsh <nwalsh@gomodus.com>
2020-09-16 10:18:55 -0700

    cleaned up the code

--- Ios Demonstrations/BeerSearch/SeachVC.swift
+++ Ios Demonstrations/BeerSearch/SeachVC.swift
@@ -18,80 +18,41 @@ public class SeachVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     var provider: MoyaProvider<BeerSearch>!
-  var issueTrackerModel: IssueTrackerModel!
 
   var latestRepositoryName : Observable<String>  {
-        return searchBar
-            .rx.text
-            .orEmpty
-            .debounce(.milliseconds(500), scheduler: MainScheduler.instance)
-            .distinctUntilChanged()
-    }
+    return searchBar
+        .rx.text
+        .orEmpty
+        .debounce(.milliseconds(500), scheduler: MainScheduler.instance)
+        .distinctUntilChanged()
+  }
   
   public override func viewDidLoad() {
-      super.viewDidLoad()
+    super.viewDidLoad()
+    tableView.register(UINib(nibName: "SearchCell", bundle: nil), forCellReuseIdentifier: "beerCell")
     setupRx()
-      tableView.register(UINib(nibName: "SearchCell", bundle: nil), forCellReuseIdentifier: "beerCell")
-//      tableView.dataSource = self
-    }
+  }
+  
   func setupRx() {
     provider = MoyaProvider<BeerSearch>()
     issueTrackerModel = IssueTrackerModel();
-    let result = issueTrackerModel.findBeer(query: "")
-//    provider.rx.request(BeerSearch.getBeer("Movember")).map(to: [Beer].self).subscribe { event in
-//              switch event {
-//              case .success(let user):
-//                  print(user)
-//              case .error(let error):
-//                  print(error)
-//              }
-//      }
-
     latestRepositoryName
        .observeOn(MainScheduler.instance)
        .flatMapLatest { name -> Observable<[Beer]> in
-           print("Name: \(name)")
          return self.provider.rx.request(BeerSearch.getBeer(name)).map(to: [Beer].self).asObservable()
        }.bind(to: tableView.rx.items) { tableView, row, item in
                                      let cell = tableView.dequeueReusableCell(withIdentifier: "beerCell", for: IndexPath(row: row, section: 0))
                                      cell.textLabel?.text = item.name
-
                                      return cell
                                  }
        .disposed(by: self.disposeBag)
-    
-//    observer.map { objects in
-//      print(objects)
-//    }
-    //            print("Repository: \(repository.fullName)")
-    //            return self.findIssues(repository: repository)
-//      observer.bind(to: tableView.rx.items) { tableView, row, item in
-//                   let cell = tableView.dequeueReusableCell(withIdentifier: "beerCell", for: IndexPath(row: row, section: 0))
-//                   cell.textLabel?.text = item.fullName
-//
-//                   return cell
-//               }
-//              .disposed(by: disposeBag)
-         // Here we tell table view that if user clicks on a cell,
-         // and the keyboard is still visible, hide it
-         tableView
-             .rx.itemSelected
-             .subscribe(onNext: { indexPath in
-                 if self.searchBar.isFirstResponder == true {
-                     self.view.endEditing(true)
-                 }
-             })
-          .disposed(by: disposeBag)
+     tableView
+         .rx.itemSelected
+         .subscribe(onNext: { indexPath in
+             if self.searchBar.isFirstResponder == true {
+                 self.view.endEditing(true)
+             }
+         })
+      .disposed(by: disposeBag)
   }
 }
-
-//extension SeachVC : UITableViewDelegate,UITableViewDataSource {
-//  public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//    return shownCities.count
-//  }
-//  public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//     let cell = tableView.dequeueReusableCell(withIdentifier: "beerCell", for: indexPath)
-//         cell.textLabel?.text = shownCities[indexPath.row]
-//         return cell
-//  }
-//}
